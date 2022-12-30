0371f55645efe1ac013d0dbfa464e1f2132ff206
Mateo Antunez <mateoantunez0@gmail.com>
2022-12-30 18:32:35 -0300

    Actualizacion backend y frontend

---
+++ ProyectoIntegrador_AntunezEnzoMateo/BackEnd/src/test/java/com/portfolio/aem/AemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.aem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
